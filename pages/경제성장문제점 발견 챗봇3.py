a6ab7b6bd7f54a604654ed59518d97c33b33070d
starrylife <forinnocen@naver.com>
2024-03-08 16:35:55 +0900

    0308_14

--- pages/경제성장문제점 발견 챗봇3.py
+++ pages/경제성장문제점 발견 챗봇3.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
 
-# 챗봇 응답을 위한 미리 할당된 공간 생성
-response_placeholder = st.empty()
-
 def configure_genai():
     gemini_api_key1 = st.secrets["gemini_api_key1"]
     genai.configure(api_key=gemini_api_key1)
@@ -29,28 +26,29 @@ def configure_genai():
 
 st.title("한국 경제성장의 숨겨진 문제점 탐색 챗봇")
 
+# 대화 내역을 표시할 컨테이너 생성
+chat_container = st.container()
+
+# 사용자 입력 받기
+user_input = st.text_input("경제성장 문제점에 대해 궁금한 점을 입력해주세요:", key="user_input")
+
 if 'convo' not in st.session_state:
     model = configure_genai()
     initial_prompt = "이 챗봇은 소크라테스 문답법을 사용하여 한국의 급격한 경제성장에 따른 문제점을 탐색하도록 도와주는 역할을 합니다. 사용자가 질문을 하면, 챗봇은 직접적인 답변 대신에 사용자로 하여금 스스로 생각하고 답을 찾을 수 있도록 가이드하는 질문을 던집니다. 이 방식으로, 사용자는 자신의 생각을 확장하고, 문제에 대한 깊이 있는 이해를 도모할 수 있습니다."
-    convo = model.start_chat(history=[{"role": "system", "content": initial_prompt}])
-    st.session_state.convo = convo
+    st.session_state.convo = model.start_chat(history=[{"role": "system", "content": initial_prompt}])
     st.session_state.chat_history = []
 
-# 사용자 입력 받기
-user_input = st.text_input("경제성장 문제점에 대해 궁금한 점을 입력해주세요:", key="user_input", on_change=handle_input)
+# 사용자가 입력을 제출한 경우 처리
+if user_input:
+    # 챗봇으로부터 응답 받기
+    response = st.session_state.convo.send_message(user_input)
+    response_text = response.text if response.text else "답변을 받지 못했습니다. 다시 시도해주세요."
 
-def handle_input():
-    user_question = st.session_state.user_input
-    if user_question:
-        st.session_state.chat_history.insert(0, f"**당신**: {user_question}")
-        response = st.session_state.convo.send_message(user_question)
-        if response.text:
-            formatted_response = f"**챗봇**: {response.text}"
-            st.session_state.chat_history.insert(0, formatted_response)
-        else:
-            st.session_state.chat_history.insert(0, "**챗봇**: 답변을 받지 못했습니다. 다시 시도해주세요.")
+    # 대화 내역 업데이트
+    st.session_state.chat_history.append(f"**당신**: {user_input}")
+    st.session_state.chat_history.append(f"**챗봇**: {response_text}")
 
-        response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
-
-if 'chat_history' not in st.session_state:
-    st.session_state.chat_history = []
+    # 대화 내역을 컨테이너에 표시
+    with chat_container:
+        for message in reversed(st.session_state.chat_history):
+            st.markdown(message)
